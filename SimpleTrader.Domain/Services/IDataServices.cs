db64510ec3b243f21bfbdafa7c139558db30e456
Vu Truong Giang <85680448+trgiangv@users.noreply.github.com>
2023-11-20 00:30:26 +0700

    Add project files.

---
+++ SimpleTrader.Domain/Services/IDataServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Services
+{
+    public interface IDataServices<T> 
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Create(T entity);
+        Task<T> Update(T entity);
+        Task<bool> Delete(int id);
+
+        
+    }
+}
